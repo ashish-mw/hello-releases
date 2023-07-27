d2ed542d41af64abd96a601748c0ce0a82893079
Ashish Kurian Thomas <ashish@mindwaveventures.com>
2023-07-27 10:22:31 +0530

    Finished adding comments
    - wip: comment feat
    - wip: add 2 comment
    - wip: 3rd commit

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const { add } = require('./lib/add')
 const { zero } = require('./lib/return-zero')
 
+// chnage one
+// change two
+// change three
 console.log('Add: ', add(1,2,3,4))
 console.log('Zero: ', zero())
